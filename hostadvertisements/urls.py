fc480979b34b641daaaf558680287b3947df3eca
Mahbub2001 <imahbubturza@gmail.com>
2024-06-15 23:55:40 +0600

    update_filter

--- hostadvertisements/urls.py
+++ hostadvertisements/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import AdvertisementViewSet
+from .views import AdvertisementViewSet,PublicAdvertisementViewSet
 
 router = DefaultRouter()
 router.register(r'advertisements', AdvertisementViewSet)
+router.register(r'public/advertisements', PublicAdvertisementViewSet, basename='public-advertisements')
+
 
 urlpatterns = [
     path('', include(router.urls)),
--- hostadvertisements/views.py
+++ hostadvertisements/views.py
@@ -1,10 +1,11 @@
-from rest_framework import viewsets, status
+from rest_framework import viewsets, status, filters
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.authentication import TokenAuthentication
 from .models import Advertisement
 from .serializers import AdvertisementSerializer
 from rest_framework.exceptions import ValidationError
+from django.db.models import Q
 
 class AdvertisementViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated] 
@@ -35,3 +36,30 @@ class AdvertisementViewSet(viewsets.ModelViewSet):
         instance = self.get_object()  
         self.perform_destroy(instance)  
         return Response(status=status.HTTP_204_NO_CONTENT) 
+
+
+class PublicAdvertisementViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Advertisement.objects.all()
+    serializer_class = AdvertisementSerializer
+    permission_classes = [] 
+    authentication_classes = []  
+
+    # Add filtering capabilities
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['title', 'city', 'country']
+    ordering_fields = ['title', 'city', 'country']
+
+    def get_queryset(self):
+        queryset = self.queryset
+        title = self.request.query_params.get('title', None)
+        city = self.request.query_params.get('city', None)
+        country = self.request.query_params.get('country', None)
+
+        if title:
+            queryset = queryset.filter(title__icontains=title)
+        if city:
+            queryset = queryset.filter(city__icontains=city)
+        if country:
+            queryset = queryset.filter(country__icontains=country)
+
+        return queryset
