e846bfecd88274b4b26d02ceffe7a5f48a904600
Mahbub2001 <imahbubturza@gmail.com>
2024-06-19 17:49:59 +0600

    udpate

--- bookings/models.py
+++ bookings/models.py
@@ -7,6 +7,10 @@ class Booking(models.Model):
     item = models.ForeignKey(Advertisement, on_delete=models.CASCADE, related_name='bookings')
     booking_date = models.DateTimeField(auto_now_add=True)
     is_completed = models.BooleanField(default=False)
+    number_of_persons = models.IntegerField(default=1)
+    from_date = models.DateField()
+    to_date = models.DateField()
+    subtotal = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
         return f'Booking {self.id} by {self.user.username} for {self.item.title}'
