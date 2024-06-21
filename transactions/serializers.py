20428cb15ed834ac24b0252ec9d87e9695c4cc16
Mahbub2001 <imahbubturza@gmail.com>
2024-06-22 01:47:25 +0600

    stripe_payment

--- transactions/serializers.py
+++ transactions/serializers.py
@@ -4,4 +4,4 @@ from .models import Transaction
 class TransactionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Transaction
-        fields = ['id', 'user', 'item', 'transaction_date', 'amount', 'is_successful']
+        fields = ['id', 'user', 'item', 'transaction_date', 'amount', 'is_successful','amount','is_successful','stripe_payment_intent_id','stripe_charge_id']
--- transactions/views.py
+++ transactions/views.py
@@ -1,10 +1,13 @@
+import stripe
 from rest_framework import viewsets, status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from django.conf import settings
 from .models import Transaction
 from .serializers import TransactionSerializer
-from hostadvertisements.models import Advertisement
 from bookings.models import Booking
+
+stripe.api_key = settings.STRIPE_SECRET_KEY  
 
 class TransactionViewSet(viewsets.ModelViewSet):
     queryset = Transaction.objects.all()
@@ -26,12 +29,39 @@ class TransactionViewSet(viewsets.ModelViewSet):
         if Transaction.objects.filter(item=item, is_successful=True).exists():
             return Response({"detail": "This item has already been successfully booked and paid for by another user."}, status=status.HTTP_400_BAD_REQUEST)
 
-        transaction = serializer.save()
+        try:
+            intent = stripe.PaymentIntent.create(
+                amount=int(serializer.validated_data['amount'] * 100),  # Convert amount to cents
+                currency='usd',
+                metadata={'integration_check': 'accept_a_payment'},
+            )
+            data['stripe_payment_intent_id'] = intent['id']
+        except stripe.error.StripeError as e:
+            return Response({"detail": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
-        if transaction.is_successful:
-            item.availability = False
-            item.save()
-            Booking.objects.filter(item=item, is_completed=False).update(is_completed=True)
+        transaction = serializer.save()
 
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        data = request.data.copy()
+
+        if 'is_successful' in data and data['is_successful']:
+            intent_id = instance.stripe_payment_intent_id
+            try:
+                intent = stripe.PaymentIntent.confirm(intent_id)
+                data['stripe_charge_id'] = intent['charges']['data'][0]['id']
+            except stripe.error.StripeError as e:
+                return Response({"detail": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+        serializer = self.get_serializer(instance, data=data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        if getattr(instance, '_prefetched_objects_cache', None):
+            instance._prefetched_objects_cache = {}
+
+        return Response(serializer.data)
